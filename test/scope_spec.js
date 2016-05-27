bbaada56453e2f2fe55bb80b60087dfe0f883908
Chris Bassano <christo4b@gmail.com>
2016-05-26 18:05:34 -0700

    removed grunt, added jshintrc

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -14,3 +14,24 @@ describe("Scope", function(){
 
 });
 
+// Digest will iterate over all of the watchers and run their watch and listen functions
+describe("digest", function(){
+
+  var scope;
+
+  beforeEach(function(){
+    scope = new Scope();
+  });
+
+  it("calls the listener function of a watch on first $digest", function(){
+    var watchFn = function() { return 'wat' ;};
+    var listenerFn = jasmine.createSpy();
+    
+    // Invoke $watch to register a watcher on the scope.
+    // Pass a jasmine Spy as the listener function
+    scope.$watch(watchFn, listenerFn);
+    scope.$digest();
+    expect(listenerFn).toHaveBeenCalled();
+  });
+
+});
